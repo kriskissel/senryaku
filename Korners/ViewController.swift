59aa3cb679febd42d50863c4fef7aa68a210bd14
Kris Kissel <kakst50ultra@hotmail.com>
2015-10-28 10:37:56 -0700

    Initial Commit

---
+++ Korners/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Korners
+//
+//  Created by Kris Kissel on 10/28/15.
+//  Copyright © 2015 Kris Kissel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
