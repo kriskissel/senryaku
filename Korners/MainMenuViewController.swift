fcf9cf7e4a9a8fc5e0975557b67a5be9bb3a777f
Kris Kissel <kakst50ultra@hotmail.com>
2015-10-28 12:59:24 -0700

    Build main menu view

---
+++ Korners/MainMenuViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MainMenuViewController.swift
+//  Korners
+//
+//  Created by Kris Kissel on 10/28/15.
+//  Copyright © 2015 Kris Kissel. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    
+    
+    @IBOutlet weak var levelSelectorSegmentedControl: UISegmentedControl!
+    
+    
+    @IBAction func playNow(sender: UIButton) {
+        print("Pressed Play Now")
+        print("Current level: \(levelSelectorSegmentedControl.selectedSegmentIndex)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
